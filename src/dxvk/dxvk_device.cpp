26477b0ca6a838c9cd5f688e7e07a13d4d8ea03a
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2018-11-17 10:18:32 +0100

    [dxvk] Create new query pool and forward it to the context

--- src/dxvk/dxvk_device.cpp
+++ src/dxvk/dxvk_device.cpp
@@ -18,6 +18,7 @@ namespace dxvk {
     m_renderPassPool    (new DxvkRenderPassPool     (vkd)),
     m_pipelineManager   (new DxvkPipelineManager    (this, m_renderPassPool.ptr())),
     m_gpuEventPool      (new DxvkGpuEventPool       (vkd)),
+    m_gpuQueryPool      (new DxvkGpuQueryPool       (vkd)),
     m_metaClearObjects  (new DxvkMetaClearObjects   (vkd)),
     m_metaCopyObjects   (new DxvkMetaCopyObjects    (vkd)),
     m_metaMipGenObjects (new DxvkMetaMipGenObjects  (vkd)),
@@ -131,6 +132,7 @@ namespace dxvk {
     return new DxvkContext(this,
       m_pipelineManager,
       m_gpuEventPool,
+      m_gpuQueryPool,
       m_metaClearObjects,
       m_metaCopyObjects,
       m_metaMipGenObjects,
@@ -141,6 +143,14 @@ namespace dxvk {
 
   Rc<DxvkGpuEvent> DxvkDevice::createGpuEvent() {
     return new DxvkGpuEvent(m_vkd);
+  }
+
+
+  Rc<DxvkGpuQuery> DxvkDevice::createGpuQuery(
+          VkQueryType           type,
+          VkQueryControlFlags   flags,
+          uint32_t              index) {
+    return new DxvkGpuQuery(m_vkd, type, flags, index);
   }
   
   
